bf7237fdcb0a999fa494837f25dbfe31547b4adb
alf <alf808>
2015-06-18 20:14:39 -1000

    commit marker model
    got data from google maps google doc

--- app/scripts/model.js
+++ app/scripts/model.js
@@ -25,7 +25,33 @@ function neighborhoodMapViewModel() {
 		lng = latAndLng.lng();
 	}
 
-	/**
+	var Pin = function Pin(map, name, lat, lon, text) {
+  var marker;
+
+  this.name = ko.observable(name);
+  this.lat  = ko.observable(lat);
+  this.lon  = ko.observable(lon);
+  this.text = ko.observable(text);
+
+  marker = new google.maps.Marker({
+    position: new google.maps.LatLng(lat, lon),
+    animation: google.maps.Animation.DROP
+  });
+
+  this.isVisible = ko.observable(false);
+
+  this.isVisible.subscribe(function(currentState) {
+    if (currentState) {
+      marker.setMap(map);
+    } else {
+      marker.setMap(null);
+    }
+  });
+
+  this.isVisible(true);
+};
+
+/**
 	* The CenterControl adds a control to the map that recenters the map on Chicago.
 	* This constructor takes the control DIV as an argument.
 	* constructor
@@ -127,6 +153,10 @@ function neighborhoodMapViewModel() {
 				window.clearTimeout(timer);
 			});
 }
+// end of initialize
+//
+//
+// self.queryName = ko.observable('');
 
 		// Will let the user know when Google Maps fails to load.
 		function failedToLoad() {
