dcb38952ceb98cd53cae5262ae78d535d0f28037
alf <alf808>
2015-06-22 17:00:18 -1000

    created info window dom

--- app/scripts/model.js
+++ app/scripts/model.js
@@ -1,5 +1,4 @@
 var map;
-var infowindow;
 
 function neighborhoodMapViewModel() {
 	var self = this;
@@ -13,16 +12,17 @@ function neighborhoodMapViewModel() {
 	// array to hold info for knockout
 	self.pins = ko.observableArray([]);
 	self.query = ko.observable('');
+	// self.googleinfowintext = '';
 
 	// string to hold foursquare information
 	self.foursquareInfo = '';
 	/**
 	* http://stackoverflow.com/questions/29557938/removing-map-pin-with-search
 	*/
-	var Pin = function Pin(map, name, lat, lon, id) {
+	var Pin = function Pin(map, gname, lat, lon, id) {
 		var marker;
 
-		this.name = ko.observable(name);
+		this.googlename = ko.observable(gname);
 		this.lat  = ko.observable(lat);
 		this.lon  = ko.observable(lon);
 
@@ -45,12 +45,12 @@ function neighborhoodMapViewModel() {
 		this.isVisible(true);
 
 		// createMarker(marker,id,this);
-		var contentString = 'gname';
+		var googleinfowintext = gname + '<br>' + self.foursquareInfo;
 
 		google.maps.event.addListener(marker, 'click', function() {
-			if (infowindow) infowindow.close();
-			infowindow.setContent(contentString);
-			infowindow.open(map, marker);
+			if (googleinfowin) googleinfowin.close();
+			googleinfowin.setContent(googleinfowintext);
+			googleinfowin.open(map, marker);
 			map.panTo(marker.position);
 			marker.setAnimation(google.maps.Animation.BOUNCE);
 			setTimeout(function(){marker.setAnimation(null);}, 1000);
@@ -84,23 +84,23 @@ function neighborhoodMapViewModel() {
 		controlDiv.appendChild(controlUI);
 		var controlText = document.createElement('div');
 		controlText.className = "reset-button-text";
-		controlText.innerHTML = 'Recenter Kapahulu Sites';
+		controlText.innerHTML = 'Recenter Kapahulu Map';
 		controlUI.appendChild(controlText);
 
 		// Setup the click event listener to simply reset the map to kapahulu
 		google.maps.event.addDomListener(controlUI, 'click', function() {
 			map.setCenter(kapahulu);
 			map.setZoom(16);
-			if (infowindow) infowindow.close();
+			if (googleinfowin) googleinfowin.close();
 			self.query('');
 		});
 	}
-
 	/*
 	Loads the map as well as position the search bar and list. On a search, clearMarkers removes all markers already on the map and removes all info in allPlaces. Then, once a search is complete, populates more markers and sends the info to getAllPlaces to populate allPlaces again. Zoom level 0-19; 0 for a planetary view and 19 to a very local view
 	*/
 	self.list = (document.getElementById('list'));
 	var input = (document.getElementById('pac-input'));
+
 	var mapDiv = document.getElementById('map-canvas');
 	var mapOptions = {
 		zoom: 16,
@@ -124,20 +124,28 @@ function neighborhoodMapViewModel() {
 
 		map = new google.maps.Map(mapDiv, mapOptions);
 		service = new google.maps.places.PlacesService(map);
-		getPlaces();
+		getGooglePlaces();
 		// Create the DIV to hold the control and call the CenterControl() constructor
 		map.controls[google.maps.ControlPosition.TOP_RIGHT].push(self.list);
 
 		var centerControlDiv = document.createElement('div');
 		var centerControl = new CenterControl(centerControlDiv, map);
 		centerControlDiv.index = 1;
-		map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(centerControlDiv);
+		map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(centerControlDiv);
 		map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
+		// node element for googles info window
+// google.maps.event.addListener(googleinfowin, 'domready', function() {
+// 		infowindowDiv.innerHTML = 'hello';
+// });
+// var infowindowDiv = document.createElement('div');
+// infowindowDiv.className = "googleinfowindow";
+// infowindowDiv.innerHTML = "";
 		// Handles an event where Google Maps takes too long to load
 		var timer = window.setTimeout(failedToLoad, 8000);
 		google.maps.event.addListener(map, 'tilesloaded', function() {
 			window.clearTimeout(timer);
+			// maybe add option for googleinfowindow.close();
 		});
 	}
 	// Posts a message to let user know when Google Maps fails to load.
@@ -147,26 +155,22 @@ function neighborhoodMapViewModel() {
 	// This interfaces to bound pin markers in view
 	self.filterPins = ko.computed(function () {
 			var search  = self.query().toLowerCase();
-
 			return ko.utils.arrayFilter(self.pins(), function (pin) {
-					var doesMatch = pin.name().toLowerCase().indexOf(search) >= 0;
-
+					var doesMatch = pin.googlename().toLowerCase().indexOf(search) >= 0;
 					pin.isVisible(doesMatch);
-
 					return doesMatch;
 			});
 	});
 	/*
 	Function to pre-populate the map -- nearbySearch retuns up to 20 places.
 	*/
-	function getPlaces() {
+	function getGooglePlaces() {
 		var request = {
 			location: kapahulu,
 			radius: 500,
 			types: ['restaurant', 'cafe', 'food'],
 		};
-
-		infowindow = new google.maps.InfoWindow();
+		googleinfowin = new google.maps.InfoWindow();
 		service.nearbySearch(request, callback);
 	}
 	/*
@@ -195,9 +199,13 @@ function neighborhoodMapViewModel() {
 		var getFoursquareInfoDetail;
 		var marker = place.marker();
 
-		var contentString = '<div style="font-weight: bold">' + place.name() + '</div>' + self.foursquareInfo;
-		infowindow.setContent(contentString);
-		infowindow.open(map, marker);
+		var googleinfowintext = place.googlename();
+		var infowindowDiv = document.createElement('div');
+		infowindowDiv.innerHTML = googleinfowintext + 'hi';
+		// container.appendChild(infowindowDiv);
+
+		googleinfowin.setContent(infowindowDiv);
+		googleinfowin.open(map, marker);
 		map.panTo(pos);
 		marker.setAnimation(google.maps.Animation.BOUNCE);
 		setTimeout(function(){marker.setAnimation(null);}, 1000);
