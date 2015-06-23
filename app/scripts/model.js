7f704cd90dbd22915f6a624424af318c13ae882d
alf <alf808>
2015-06-22 23:49:59 -1000

    backing up. no success with infowindow

--- app/scripts/model.js
+++ app/scripts/model.js
@@ -1,8 +1,11 @@
 var map;
+// var foursquareInfo;
+var fsidArray = [];
 
 function neighborhoodMapViewModel() {
 	var self = this;
 	var service;
+	var googleinfowin;
 
 	// kapahulu area's latitude and longitude
 	var latitude = 21.2790587;
@@ -11,6 +14,7 @@ function neighborhoodMapViewModel() {
 
 	// array to hold info for knockout
 	self.pins = ko.observableArray([]);
+	// self.fsidArray = [];
 	self.query = ko.observable('');
 	// self.googleinfowintext = '';
 
@@ -19,8 +23,9 @@ function neighborhoodMapViewModel() {
 	/**
 	* http://stackoverflow.com/questions/29557938/removing-map-pin-with-search
 	*/
-	var Pin = function Pin(map, gname, lat, lon, id) {
+	var Pin = function(map, gname, lat, lon, id) {
 		var marker;
+		// self.getFoursquareInfo(lat, lon);
 
 		this.googlename = ko.observable(gname);
 		this.lat  = ko.observable(lat);
@@ -30,7 +35,6 @@ function neighborhoodMapViewModel() {
 			icon: 'img/red-dot.png',
 			position: new google.maps.LatLng(lat, lon),
 			place_id: id
-			// animation: google.maps.Animation.DROP
 		});
 		this.marker = ko.observable(marker);
 		this.isVisible = ko.observable(false);
@@ -43,19 +47,22 @@ function neighborhoodMapViewModel() {
 			}
 		});
 		this.isVisible(true);
+		// createMarker
+		//
+		// this.gfi = ko.computed(function() {
+		// 	getFoursquareInfo(lat, lon);
+	  // }, this);
 
-		// createMarker(marker,id,this);
-		var googleinfowintext = gname + '<br>' + self.foursquareInfo;
-
-		google.maps.event.addListener(marker, 'click', function() {
-			if (googleinfowin) googleinfowin.close();
-			googleinfowin.setContent(googleinfowintext);
-			googleinfowin.open(map, marker);
-			map.panTo(marker.position);
-			marker.setAnimation(google.maps.Animation.BOUNCE);
-			setTimeout(function(){marker.setAnimation(null);}, 1000);
-		});
-
+		// google.maps.event.addListener(marker, 'click', function() {
+		// 	var googleinfowintext = '<br>' + this.gfi();
+		// 	if (googleinfowin) googleinfowin.close();
+		// 	googleinfowin.setContent(googleinfowintext);
+		// 	googleinfowin.open(map, marker);
+		// 	map.panTo(marker.position);
+		// 	marker.setAnimation(google.maps.Animation.BOUNCE);
+		// 	setTimeout(function(){marker.setAnimation(null);}, 1000);
+		// });
+		// this.testfunction: function {console.log('hello');}
 	};
 	/**
 	* This should help asynchronously execute functions. It will be used to fetch
@@ -84,7 +91,7 @@ function neighborhoodMapViewModel() {
 		controlDiv.appendChild(controlUI);
 		var controlText = document.createElement('div');
 		controlText.className = "reset-button-text";
-		controlText.innerHTML = 'Recenter Kapahulu Map';
+		controlText.innerHTML = 'Reset Kapahulu Map';
 		controlUI.appendChild(controlText);
 
 		// Setup the click event listener to simply reset the map to kapahulu
@@ -96,10 +103,10 @@ function neighborhoodMapViewModel() {
 		});
 	}
 	/*
-	Loads the map as well as position the search bar and list. On a search, clearMarkers removes all markers already on the map and removes all info in allPlaces. Then, once a search is complete, populates more markers and sends the info to getAllPlaces to populate allPlaces again. Zoom level 0-19; 0 for a planetary view and 19 to a very local view
+	Loads the map as well as position the search bar and list, populates more markers. Zoom level 0-19; 0 for a planetary view and 19 to a very local view
 	*/
-	self.list = (document.getElementById('list'));
-	var input = (document.getElementById('pac-input'));
+	self.list = document.getElementById('list');
+	var input = document.getElementById('pac-input');
 
 	var mapDiv = document.getElementById('map-canvas');
 	var mapOptions = {
@@ -123,7 +130,6 @@ function neighborhoodMapViewModel() {
 	function initialize() {
 
 		map = new google.maps.Map(mapDiv, mapOptions);
-		service = new google.maps.places.PlacesService(map);
 		getGooglePlaces();
 		// Create the DIV to hold the control and call the CenterControl() constructor
 		map.controls[google.maps.ControlPosition.TOP_RIGHT].push(self.list);
@@ -134,18 +140,14 @@ function neighborhoodMapViewModel() {
 		map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(centerControlDiv);
 		map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-		// node element for googles info window
-// google.maps.event.addListener(googleinfowin, 'domready', function() {
-// 		infowindowDiv.innerHTML = 'hello';
-// });
-// var infowindowDiv = document.createElement('div');
-// infowindowDiv.className = "googleinfowindow";
-// infowindowDiv.innerHTML = "";
 		// Handles an event where Google Maps takes too long to load
 		var timer = window.setTimeout(failedToLoad, 8000);
 		google.maps.event.addListener(map, 'tilesloaded', function() {
 			window.clearTimeout(timer);
-			// maybe add option for googleinfowindow.close();
+			// googleinfowin.close();
+		});
+		google.maps.event.addListener(map, 'click', function() {
+			googleinfowin.close();
 		});
 	}
 	// Posts a message to let user know when Google Maps fails to load.
@@ -171,6 +173,7 @@ function neighborhoodMapViewModel() {
 			types: ['restaurant', 'cafe', 'food'],
 		};
 		googleinfowin = new google.maps.InfoWindow();
+		service = new google.maps.places.PlacesService(map);
 		service.nearbySearch(request, callback);
 	}
 	/*
@@ -181,46 +184,61 @@ function neighborhoodMapViewModel() {
 			// bounds = new google.maps.LatLngBounds();
 			results.forEach(function (place){
 				var lat = place.geometry.location.lat();
-				var lng = place.geometry.location.lng();
-
-				var pin = new Pin(map, place.name, lat, lng, place.place_id);
+				var lon = place.geometry.location.lng();
+				var marker = getFoursquareInfo(lat, lon);
+				var pin = new Pin(map, place.name, lat, lon, place.place_id);
 				// bounds.extend(new google.maps.LatLng(latitude,longitude));
+				getFoursquareIdList(lat, lon);
 				self.pins.push(pin);
 			});
-			// console.log(results);
+			console.log(self.fsidArray);
 			// map.fitBounds(bounds);
 		}
 	}
 
-	self.getFoursquareInfo = function(lat, lon) {
+	function getFoursquareIdList(lat, lon) {
 		var foursquareURL = 'http://api.foursquare.com/v2/venues/search?ll=' +lat+ ',' +lon+ '&oauth_token=GQDPA05ROIS0UO5KO3YQEW4KGYBC2QOW1PCKD0HMQR5COFVH&v=20150830&m=foursquare';
 		$.getJSON(foursquareURL, function(data) {
-			self.foursquareInfo = 'yeah '+data.response.venues[0].name;
-			console.log('yeah '+data.response.venues[0].id);
+			// console.log('yeah '+data.response.venues[0].name);
+			var fsqid = data.response.venues[0].id;
+			fsidArray.push(fsqid);
 		}).error(function(e){
 			console.log('oops');
 		});
-	};
+	}
+
+	function getFoursquareInfo(lat, lon) {
+		var foursquareURL = 'http://api.foursquare.com/v2/venues/search?ll=' +lat+ ',' +lon+ '&oauth_token=GQDPA05ROIS0UO5KO3YQEW4KGYBC2QOW1PCKD0HMQR5COFVH&v=20150830&m=foursquare';
+		$.getJSON(foursquareURL, function(data) {
+			console.log('yeah '+data.response.venues[0].id);
+			self.foursquareInfo = data.response.venues[0].name;
+			// self.fsidArray.push(fsqid);
+		}).error(function(e){
+			console.log('oops');
+		});
+	}
+
 	/*
 	Function that will open info window of an item clicked in the list.
 	*/
 	self.clickMarker = function(place) {
-		self.getFoursquareInfo(place.lat(), place.lon());
+		// var gfi = getFoursquareInfo(place.lat(), place.lon());
 		var pos = new google.maps.LatLng(place.lat(), place.lon());
 		// var getFoursquareInfoDetail;
 		var marker = place.marker();
+		var googleinfowintext = place.googlename() +'<br>'+ self.foursquareInfo;
 
 		map.panTo(pos);
 		// wait for a few milliseconds to get info from foursquare
 		setTimeout(function() {
-			var googleinfowintext = place.googlename() +'<br>'+ self.foursquareInfo;
+			if (googleinfowin) googleinfowin.close();
 			var infowindowDiv = document.createElement('div');
 			infowindowDiv.innerHTML = googleinfowintext;
 			googleinfowin.setContent(infowindowDiv);
 			googleinfowin.open(map, marker);
 			marker.setAnimation(google.maps.Animation.BOUNCE);
 			setTimeout(function(){marker.setAnimation(null);}, 1000);
-		}, 400);
+		}, 500);
 
 	};
 
