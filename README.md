fbc7b6ca107d349d3825be27de0d5e972c94036f
alf <alf@akademe.net>
2015-06-26 19:29:25 -1000

    commit before merging with master

--- README.md
+++ README.md
@@ -4,48 +4,37 @@
 
 1. Although separation of concerns has already been intimated in previous projects -- from styling separation in project 1, JSON models in project 2, pseudo-classical object-orientation in project 3, and even in the optimization project with DOM manipulation in JS code --, it's not until project 5 in which it was explicit that the view and the controller be separated as illustrated in Ben Jaffe's JavaScript Design Patterns course.
 
-1. The task was to use knockout to illustrate the MVVM principle in that we had to bind variables in the view which was manipulated in the viewModel JS code. My code starts with a Pin class which is basically the app's sole model which corresponds to google map markers in the view.
+1. The task was to use knockout to illustrate the MVVM principle in that we had to bind variables in the view which was manipulated in the viewModel. My code starts with a Pin class which is basically the application's sole model which corresponds to google map markers in the view.
 
-1. Google Map's InfoWindow does not lend itself to styling. As much as I wanted to eliminate the HTML tags in the viewModel, the combination of the async request and the DOM manipulation required a lot of waiting time producing inaccurate results on click.
+1. There are some functions that could have been encapsulated in the Pin class as it was required in the OOP project. However scoping issues came up when I attempted to use the pseudo-classical OOP structure with prototypal methods when using Knockout. I need to do more research to accomplish this.
 
-### Part 1
+1. Google Map's InfoWindow does not lend itself easily to styling. It is possible to style it. I prepared my code to do a DOM manipulation in case it be required in the future to style InfoWindow more intricately.
 
-#### Getting started: Things done before optimization begins
+### Functionality of the Neighborhood application
 
-1. Checked out the repository
+1. The application has the required components of a search bar, a list view and a Google map. The search bar has the feature to filter locations in the list view and the pin markers on the map. As user clicks on a pin marker or a venue name on the list, a Google InfoWindow pops up with Google and Foursquare data.
 
-1. Installed nodejs and npm
+1. As the user loads the application, the Google markers are loaded both statically and dynamically. I initially coded the app with Google Map's Nearby Search (https://developers.google.com/maps/documentation/javascript/places#place_search_requests). I eventually discovered that the latitude and longitude do not match with those of Foursquare's. I used the code with Nearby Search to extract the latitude and longitude of venues in my vicinity. I then stored the location data in an array along with Foursquare venue IDs.
 
-1. Installed bower (thru npm install -g within app folder) for web app dependencies;
-  did bower init to create bower.json config and bower_components directory within app folder;
-  used bower [...] --save to edit bower.json config
+1. The Foursquare data is fetched using async requests to its venues/VENUE_ID API (https://developer.foursquare.com/docs/venues/venues). It is a no-nonsense straightforward API that only required an oauth_token. I used jQuery's getJSON method and a callback method so that while Foursquare data are being fetched asynchronously, the code can continue to execute.
 
-1. Installed knockout (thru bower) for JavaScript mvc framework. I did not have to use for this project but I wanted to practice use of bower for upcoming projects.
+1. There is one Reset control that recenters the map to my vicinity, clears the search bar, and places all the pin markers back to the original positions.
 
-1. npm init within project folder
-  used npm install [...] --save-dev to edit package.json config and install locally in project
+### Code base
 
-1. installed GULP and gulp plugins npm install [...] --save-dev. This is to automate optimization process, e.g. image compression and minimification of CSS, JavaScript, and HTML files
+#### This code used 3 sources for its basis
 
-#### Optimize PageSpeed Insights measurements
+1. Pin class from http://stackoverflow.com/questions/29557938/removing-map-pin-with-search
+1. Google map code from my Udacity Project 2 resume project.
+1. Google's code at https://developers.google.com/maps/documentation/javascript/examples/control-custom
 
-1. To inspect the site on your phone, you can run a local server
-  ```bash
-  $> cd /path/to/your-project-folder
-  $> python -m SimpleHTTPServer 8080
-  ```
+### Heavily Used Resources
 
-1. Open a browser and visit localhost:8080
-
-1. Download and install [ngrok](https://ngrok.com/) to make your local server accessible remotely.
-  ``` bash
-  $> cd /path/to/your-project-folder
-  $> ngrok 8080
-  ```
-
-
-### Customization with Bootstrap
-The portfolio was built on Twitter's <a href="http://getbootstrap.com/">Bootstrap</a> framework. All custom styles are in `dist/css/portfolio.css` in the portfolio repo.
-
-* <a href="http://getbootstrap.com/css/">Bootstrap's CSS Classes</a>
-* <a href="http://getbootstrap.com/components/">Bootstrap's Components</a>
+* <a href="https://github.com/udacity/fend-office-hours/tree/master/Javascript%20Design%20Patterns">Udacity's Javascript Design Patterns documentation</a>
+* <a href="https://developers.google.com/maps/documentation/javascript/">Google Maps JavaScript API</a>
+* <a href="http://knockoutjs.com/documentation/introduction.html">Knockout JS Documentation</a>
+* <a href="https://developer.foursquare.com/docs/">Foursquare's API docs</a>
+* <a href="https://github.com/knockout/knockout/wiki/asynchronous-dependent-observables">An explanation of Asynchronous calls with Knockout</a>
+* <a href="http://api.jquery.com/category/ajax/">jQuery's AJAX documentation</a>
+* <a href="stackoverflow.com">Innumerable resources from stackoverflow.com</a>
+* Udacity's AJAX course, Ben Jaffe's Javascript Design Patterns, an office hour with Udacity coach John on June 24 2015, and an appointment with Udacity coach John on June 25 2015.
--- app/scripts/application.js
+++ app/scripts/application.js
@@ -1,33 +1,33 @@
-var map;
-
+var map; // sole global variable
+/**
+* This is viewModel that interfaces with the view in index.html
+*/
 function neighborhoodMapViewModel() {
 	var self = this;
 	var service;
 	var googleinfowin;
 	var foursquareData = [];
 
-	// kapahulu area's latitude and longitude
+	// Kapahulu area's latitude and longitude
 	var latitude = 21.2790587;
 	var longitude = -157.81368810000004;
 	var kapahulu = new google.maps.LatLng(latitude, longitude);
 
-	// array to hold info for knockout
+	// These variables help keep track of the visible markers and list of venues
 	self.pins = ko.observableArray([]);
 	self.query = ko.observable('');
-
 	/**
-	* The basis of this code is the Pin class below. It is based on code from
+	* The sole model in this application -- the Pin class. It is based on code from
 	* http://stackoverflow.com/questions/29557938/removing-map-pin-with-search
 	*/
 	var Pin = function(map, gname, lat, lon, fsid) {
-		// var fsqinfodetail;
 		var self = this;
 		var marker;
+		// The observables below are bound to the list in the view
 		self.fsqid = ko.observable(fsid);
 		self.googlename = ko.observable(gname);
 		self.lat  = ko.observable(lat);
 		self.lon  = ko.observable(lon);
-		// self.idx = ko.observable(idx);
 
 		marker = new google.maps.Marker({
 			icon: 'img/red-dot.png',
@@ -45,17 +45,20 @@ function neighborhoodMapViewModel() {
 		});
 		self.isVisible(true);
 
-		// createMarker
+		// This adds the click listeners on each of the pin markers on the map
 		google.maps.event.addListener(marker, 'click', function() {
 			var fsdata = getFoursquareFromArray(fsid);
 			map.panTo(marker.position);
 			prepareInfowin(gname,marker,fsdata);
 		});
 	};
-
+	/**
+	* This function prepares the infowindow's presentation both in the pins and the list.
+	* It is possible to separate the styling of the infowindow if need be.
+	*/
 	function prepareInfowin(gname,marker,fsdata) {
-		var fsqinfodetail = fsdata[1] + '<br>' + fsdata[2] + '<br>' + fsdata[3] + '<br>' + fsdata[4] + '<br>' + fsdata[5];
-		var googleinfowintext = gname + '<br>Foursquare Info:<br>' + fsqinfodetail;
+		var fsqinfodetail = fsdata[1] + '<br>' + fsdata[2] + '<br>' + fsdata[3] + '<br>Rating: ' + fsdata[4] + '<br>URL: ' + fsdata[5];
+		var googleinfowintext = '<strong>' + gname + '</strong>' + '<hr>FOURSQUARE Info:<br>' + fsqinfodetail;
 		if (googleinfowin) googleinfowin.close();
 		var infowindowDiv = document.createElement('div');
 		infowindowDiv.innerHTML = googleinfowintext;
@@ -66,7 +69,6 @@ function neighborhoodMapViewModel() {
 	}
 	/**
 	* The CenterControl adds a control to the map that recenters the map on Neighborhood.
-	* This constructor takes the control DIV as an argument.
 	*/
 	function CenterControl(controlDiv, map) {
 
@@ -92,7 +94,6 @@ function neighborhoodMapViewModel() {
 	*/
 	self.list = document.getElementById('list');
 	var input = document.getElementById('pac-input');
-
 	var mapDiv = document.getElementById('map-canvas');
 	var mapOptions = {
 		zoom: 16, maxZoom: 16, minZoom: 10,
@@ -111,7 +112,6 @@ function neighborhoodMapViewModel() {
 	};
 
 	function initialize() {
-
 		map = new google.maps.Map(mapDiv, mapOptions);
 		getGooglePlaces();
 		// Create the DIV to hold the control and call the CenterControl() constructor
@@ -136,7 +136,10 @@ function neighborhoodMapViewModel() {
 	function failedToLoad() {
 		$('#map-canvas').html("<h1>Google Maps Failed to Load. Please try reloading the page.</h1>");
 	}
-	// This interfaces to bound pin markers in view
+	/**
+	*  This interfaces to bound pin markers in view helping the Pin model to show or hide pins
+	*  as user filters the location in search bar
+	*/
 	self.filterPins = ko.computed(function () {
 			var search  = self.query().toLowerCase();
 			return ko.utils.arrayFilter(self.pins(), function (pin) {
@@ -145,8 +148,11 @@ function neighborhoodMapViewModel() {
 					return doesMatch;
 			});
 	});
-	/*
-	Function to pre-populate the map -- nearbySearch retuns up to 20 places.
+	/**
+	*Loads the map as well as position the search bar and list, populates more markers.
+	* Zoom level 0-19; 0 for a planetary view and 19 to a very local view
+	* The function populates the map with markers with the helper data of
+	* from googleplaces array
 	*/
 	function getGooglePlaces() {
 		googleinfowin = new google.maps.InfoWindow();
@@ -162,29 +168,37 @@ function neighborhoodMapViewModel() {
 			bounds.extend(new google.maps.LatLng(latitude,longitude));
 			self.pins.push(pin);
 		}
-		// console.log(foursquareData);
 		map.fitBounds(bounds);
 		map.setCenter(bounds.getCenter());
 	}
-
+	/**
+	* The async request to Foursquare: http://api.foursquare.com/v2/venues/VENUE_ID.
+	* This is invoked as the map is being populated with pins.
+	* Callback method is being used to assist with asynchronous fetch of data.
+	* John at Udacity helped me with the syntax to make the callback work.
+	* This venues/VENUE_ID requires the Foursquare ID as part of its URL. The other
+	* parameters namely, v, m, and format are required for versioning and format return
+	*/
 	function getFoursquareDetail(fsid) {
 		var foursquareURL = 'http://api.foursquare.com/v2/venues/' + fsid + '?oauth_token=GQDPA05ROIS0UO5KO3YQEW4KGYBC2QOW1PCKD0HMQR5COFVH&v=20150830&m=foursquare&format=json';
 		$.getJSON(foursquareURL, function(data) {
-				callbackFn2(data,fsid);
+				callbackFn(data,fsid);
 		}).error(function(e){
 				console.log('oops');
 		});
 	}
-	function callbackFn2(data,fsid) {
+	// This callback pushes data to the foursquareData array which will be used at
+	// on-click demand of Foursquare data
+	function callbackFn(data,fsid) {
 		var d = data.response.venue;
 		foursquareData.push([fsid, d.contact.formattedPhone || " ", d.location.formattedAddress[0] || " ", d.location.formattedAddress[1] || " ", d.rating || " ", d.url || " "]);
 	}
-
-// SPECIFIC VENUE INFO: http://api.foursquare.com/v2/venues/VENUE_ID
+	/**
+	* This is invoked to grab foursquare data from the array using the foursquare venue_id
+	*/
 	function getFoursquareFromArray(fsid) {
 		var len = foursquareData.length;
 		var result;
-		// var d;
 		for (var i = 0; i < len; i++ ) {
 			if( foursquareData[i][0] === fsid ) {
 				result = foursquareData[i];
@@ -193,8 +207,8 @@ function neighborhoodMapViewModel() {
 		}
 		return result;
 	}
-	/*
-	Function that will open info window of an item clicked in the list.
+	/**
+	* This function will open infowindow of a venue clicked in the list.
 	*/
 	self.clickMarker = function(place) {
 		var fsdata = getFoursquareFromArray(place.fsqid());
@@ -203,7 +217,7 @@ function neighborhoodMapViewModel() {
 		map.panTo(pos);
 		prepareInfowin(place.googlename(),place.marker(),fsdata);
 	};
-
+	// The initialize function is invoked upon launching this application
 	google.maps.event.addDomListener(window, 'load', initialize);
 	window.mapBounds = new google.maps.LatLngBounds();
 	window.addEventListener('resize', function(e) {
