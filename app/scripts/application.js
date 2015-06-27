a86a6415e0d08d7632a3d3bbb619c8104ee82fdc
alf <alf@akademe.net>
2015-06-26 17:15:19 -1000

    eliminated any on-click async requests
    it was very inefficient to do async requests on-click demand. The code
    saves foursquare data in an array upon async request

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -1,91 +1,10 @@
 var map;
-var googleinfowin;
-// var fsqinfodetail;
-var foursquareData = [];
-
-/**
-* The basis of this code is the Pin class below. It is based on code below
-* http://stackoverflow.com/questions/29557938/removing-map-pin-with-search
-*/
-var Pin = function(map, gname, lat, lon, fsqid, idx) {
-	var fsqinfodetail;
-	// Function below is used to help the asynchronous fetch of Foursquare data.
-	// function callbackFn(data) {
-	// 		// fsqinfodetail = data.response.venue.name;
-	// 	var d = data.response.venue;
-	// 	fsqinfodetail = d.contact.formattedPhone + '<br>' + d.location.formattedAddress[0] + '<br>' + d.location.formattedAddress[1] + '<br>Rating: ' + d.rating + '<br>' + d.url;
-	// }
-
-	var self = this;
-	var marker;
-	// self.fsqid = ko.observable(fsqid);
-	self.googlename = ko.observable(gname);
-	self.lat  = ko.observable(lat);
-	self.lon  = ko.observable(lon);
-	self.idx = ko.observable(idx);
-
-	marker = new google.maps.Marker({
-		icon: 'img/red-dot.png',
-		position: new google.maps.LatLng(lat, lon)
-	});
-	self.marker = ko.observable(marker);
-	self.isVisible = ko.observable(false);
-
-	self.isVisible.subscribe(function(currentState) {
-		if (currentState) {
-			marker.setMap(map);
-		} else {
-			marker.setMap(null);
-		}
-	});
-	self.isVisible(true);
-
-	/**
-	* The IFFE is immediately invoked as a pin object is instantiated. It fetches
-	* information on the on a specific Foursquare venue.
-	*/
-	(function() {
-		var foursquareURL = 'http://api.foursquare.com/v2/venues/' + fsqid + '?oauth_token=GQDPA05ROIS0UO5KO3YQEW4KGYBC2QOW1PCKD0HMQR5COFVH&v=20150830&m=foursquare&format=json';
-	  $.getJSON(foursquareURL, function(data) {
-	    callbackFn(data,idx);
-	  }).error(function(e){
-	    console.log('oops');
-	  });
-	})();
-
-	// Function below is used to help the asynchronous fetch of Foursquare data.
-	function callbackFn(data,idx) {
-		var d = data.response.venue;
-		var phone = d.contact.formattedPhone || " ";
-		var street = d.location.formattedAddress[0] || " ";
-		var city = d.location.formattedAddress[1] || " ";
-		var rate = d.rating || " ";
-		var u = d.url || " ";
-		fsqinfodetail = phone + '<br>' + street + '<br>' + city + '<br>Rating: ' + rate + '<br>' + u;
-		var tarray = [phone, street, city, rate, u];
-		console.log(tarray);
-		foursquareData.push(tarray);
-	}
-
-	// createMarker
-	// NEED TO DO CLICK LISTENER HERE BUT VIOLATES dry . I wrote this below too
-	google.maps.event.addListener(marker, 'click', function() {
-		var googleinfowintext = gname + '<br>Foursquare Info:<br>' + fsqinfodetail;
-		// console.log(gname + ' ' + fsqid);
-		if (googleinfowin) googleinfowin.close();
-		googleinfowin.setContent(googleinfowintext);
-		googleinfowin.open(map, marker);
-		map.panTo(marker.position);
-		marker.setAnimation(google.maps.Animation.BOUNCE);
-		setTimeout(function(){marker.setAnimation(null);}, 1000);
-	});
-
-};
 
 function neighborhoodMapViewModel() {
 	var self = this;
 	var service;
-	var fsqinfodetail;
+	var googleinfowin;
+	var foursquareData = [];
 
 	// kapahulu area's latitude and longitude
 	var latitude = 21.2790587;
@@ -95,10 +14,56 @@ function neighborhoodMapViewModel() {
 	// array to hold info for knockout
 	self.pins = ko.observableArray([]);
 	self.query = ko.observable('');
-	// self.googleinfowintext = '';
 
-	// string to hold foursquare information
-	self.foursquareInfo = '';
+	/**
+	* The basis of this code is the Pin class below. It is based on code from
+	* http://stackoverflow.com/questions/29557938/removing-map-pin-with-search
+	*/
+	var Pin = function(map, gname, lat, lon, fsid) {
+		// var fsqinfodetail;
+		var self = this;
+		var marker;
+		self.fsqid = ko.observable(fsid);
+		self.googlename = ko.observable(gname);
+		self.lat  = ko.observable(lat);
+		self.lon  = ko.observable(lon);
+		// self.idx = ko.observable(idx);
+
+		marker = new google.maps.Marker({
+			icon: 'img/red-dot.png',
+			position: new google.maps.LatLng(lat, lon)
+		});
+		self.marker = ko.observable(marker);
+		self.isVisible = ko.observable(false);
+
+		self.isVisible.subscribe(function(currentState) {
+			if (currentState) {
+				marker.setMap(map);
+			} else {
+				marker.setMap(null);
+			}
+		});
+		self.isVisible(true);
+
+		// createMarker
+		google.maps.event.addListener(marker, 'click', function() {
+			var fsdata = getFoursquareFromArray(fsid);
+			map.panTo(marker.position);
+			prepareInfowin(gname,marker,fsdata);
+		});
+	};
+
+	function prepareInfowin(gname,marker,fsdata) {
+		var fsqinfodetail = fsdata[1] + '<br>' + fsdata[2] + '<br>' + fsdata[3] + '<br>' + fsdata[4] + '<br>' + fsdata[5];
+		var googleinfowintext = gname + '<br>Foursquare Info:<br>' + fsqinfodetail;
+		if (googleinfowin) googleinfowin.close();
+		var infowindowDiv = document.createElement('div');
+		infowindowDiv.innerHTML = googleinfowintext;
+		googleinfowin.setContent(infowindowDiv);
+		googleinfowin.open(map, marker);
+		marker.setAnimation(google.maps.Animation.BOUNCE);
+		setTimeout(function(){marker.setAnimation(null);}, 1000);
+	}
 	/**
 	* The CenterControl adds a control to the map that recenters the map on Neighborhood.
 	* This constructor takes the control DIV as an argument.
@@ -114,8 +79,7 @@ function neighborhoodMapViewModel() {
 		controlText.className = "reset-button-text";
 		controlText.innerHTML = 'Reset Kapahulu Map';
 		controlUI.appendChild(controlText);
-
-		// Setup the click event listener to simply reset the map to kapahulu
+		// Setup the click event listener to reset the map to kapahulu
 		google.maps.event.addDomListener(controlUI, 'click', function() {
 			map.setCenter(kapahulu);
 			map.setZoom(16);
@@ -187,63 +151,63 @@ function neighborhoodMapViewModel() {
 	function getGooglePlaces() {
 		googleinfowin = new google.maps.InfoWindow();
 		var bounds = window.mapBounds;
-
-		for(var i = 0; i < googleplaces.length; i++){
+		var len = googleplaces.length;
+		for(var i = 0; i < len; i++){
 			var lat = googleplaces[i][0];
 			var lon = googleplaces[i][1];
-			var fsqid = googleplaces[i][3];
+			var fsid = googleplaces[i][3];
 			var gname = googleplaces[i][2];
-			var pin = new Pin(map, gname, lat, lon, fsqid, i);
+			var pin = new Pin(map, gname, lat, lon, fsid);
+			getFoursquareDetail(fsid);
 			bounds.extend(new google.maps.LatLng(latitude,longitude));
 			self.pins.push(pin);
 		}
+		// console.log(foursquareData);
 		map.fitBounds(bounds);
 		map.setCenter(bounds.getCenter());
 	}
 
-// SPECIFIC VENUE INFO: http://api.foursquare.com/v2/venues/VENUE_ID
 	function getFoursquareDetail(fsid) {
-		var d = foursquareData[fsid];
-		// var foursquareURL = 'http://api.foursquare.com/v2/venues/' + fsid + '?oauth_token=GQDPA05ROIS0UO5KO3YQEW4KGYBC2QOW1PCKD0HMQR5COFVH&v=20150830&m=foursquare&format=json';
-		// $.getJSON(foursquareURL, function(data) {
-		// 		callbackFn2(data);
-		// }).error(function(e){
-		// 		console.log('oops');
-		// });
-		fsqinfodetail = d[0] + '<br>' + d[1] + '<br>' + d[2] + '<br>' + d[3] + '<br>' + d[4];
+		var foursquareURL = 'http://api.foursquare.com/v2/venues/' + fsid + '?oauth_token=GQDPA05ROIS0UO5KO3YQEW4KGYBC2QOW1PCKD0HMQR5COFVH&v=20150830&m=foursquare&format=json';
+		$.getJSON(foursquareURL, function(data) {
+				callbackFn2(data,fsid);
+		}).error(function(e){
+				console.log('oops');
+		});
 	}
-	// function callbackFn2(data) {
-	// 		var d = data.response.venue;
-	// 		fsqinfodetail = d.contact.formattedPhone + '<br>' + d.location.formattedAddress[0] + '<br>' + d.location.formattedAddress[1] + '<br>Rating: ' + d.rating + '<br>' + d.url;
-	// }
+	function callbackFn2(data,fsid) {
+		var d = data.response.venue;
+		foursquareData.push([fsid, d.contact.formattedPhone || " ", d.location.formattedAddress[0] || " ", d.location.formattedAddress[1] || " ", d.rating || " ", d.url || " "]);
+	}
+
+// SPECIFIC VENUE INFO: http://api.foursquare.com/v2/venues/VENUE_ID
+	function getFoursquareFromArray(fsid) {
+		var len = foursquareData.length;
+		var result;
+		// var d;
+		for (var i = 0; i < len; i++ ) {
+			if( foursquareData[i][0] === fsid ) {
+				result = foursquareData[i];
+				break;
+			}
+		}
+		return result;
+	}
 	/*
 	Function that will open info window of an item clicked in the list.
 	*/
 	self.clickMarker = function(place) {
-		getFoursquareDetail(place.idx());
+		var fsdata = getFoursquareFromArray(place.fsqid());
 		var pos = new google.maps.LatLng(place.lat(), place.lon());
-		var marker = place.marker();
 
 		map.panTo(pos);
-		if (googleinfowin) googleinfowin.close();
-		// wait for a few milliseconds to get info from foursquare.
-		setTimeout(function() {
-			var googleinfowintext = place.googlename() + '<br>Foursquare Info:<br>' + fsqinfodetail;
-			var infowindowDiv = document.createElement('div');
-			infowindowDiv.innerHTML = googleinfowintext;
-			googleinfowin.setContent(infowindowDiv);
-			googleinfowin.open(map, marker);
-			marker.setAnimation(google.maps.Animation.BOUNCE);
-			setTimeout(function(){marker.setAnimation(null);}, 1000);
-		}, 500);
-
+		prepareInfowin(place.googlename(),place.marker(),fsdata);
 	};
+
 	google.maps.event.addDomListener(window, 'load', initialize);
 	window.mapBounds = new google.maps.LatLngBounds();
-	// map.setZoom(16);
 	window.addEventListener('resize', function(e) {
 		map.fitBounds(mapBounds);
-		// map.setCenter(kapahulu);
 	});
 }
 
